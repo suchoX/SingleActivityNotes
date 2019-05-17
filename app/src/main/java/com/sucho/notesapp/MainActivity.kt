d7a22f9a394339628a34634e4f92fdf7fe337c9f
Suchandrim Sarkar <suchandrim.sarkar@gmail.com>
2019-05-17 10:31:50 +0530

    Added Toolbar with AppConfiguration

--- app/src/main/java/com/sucho/notesapp/MainActivity.kt
+++ app/src/main/java/com/sucho/notesapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.sucho.notesapp
 
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.sucho.notesapp.feature.base.BaseActivity
 import androidx.navigation.ui.setupWithNavController
@@ -14,7 +16,12 @@ class MainActivity : BaseActivity<MainActivityViewModel>() {
     setContentView(R.layout.activity_main)
 
     val navController = findNavController(R.id.my_nav_host_fragment)
+    val appBarConfiguration = AppBarConfiguration
+        .Builder(R.id.notesListFragment, R.id.highlightedNotesListFragment, R.id.newNoteFragment)
+        .build()  //Had to manually give fragments to make them top level destinations and not show the back button
     findViewById<BottomNavigationView>(R.id.bottom_nav)
         .setupWithNavController(navController)
+    findViewById<Toolbar>(R.id.toolbar)
+        .setupWithNavController(navController, appBarConfiguration)
   }
 }
